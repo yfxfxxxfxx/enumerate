242723737d5d4d79ec8828ed13a715db0f701387
yfxfxxxfxx <mail@przykladowy.co.pl>
2020-03-18 20:05:51 +0100

    Created interface for handling known extensions that cannot be identified with a magic number, current required extension meeting this criterion is .txt

---
+++ src/main/java/pl/epam/challenge/validators/TypeValidator.java
@@ -0,0 +1,11 @@
+package pl.epam.challenge.validators;
+
+/**
+ * This interface is currently only implemented by one class, because only one file type that must be
+ * verified does not have a specific magic number that can be stored in a lookup table. Interface will
+ * be useful in case additional file types are added which also require specialized verification logic.
+ */
+
+public interface TypeValidator {
+    boolean verifyFileType(byte[] bytes);
+}
