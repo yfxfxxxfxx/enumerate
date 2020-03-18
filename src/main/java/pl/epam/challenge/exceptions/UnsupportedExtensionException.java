48663513029ed03e7a65fffd5bf04feeff05629a
yfxfxxxfxx <mail@przykladowy.co.pl>
2020-03-18 19:20:04 +0100

    Created static lookup table of recognized magic numbers, and began implementing conditional logic based on file extension

---
+++ src/main/java/pl/epam/challenge/exceptions/UnsupportedExtensionException.java
@@ -0,0 +1,7 @@
+package pl.epam.challenge.exceptions;
+
+public class UnsupportedExtensionException extends RuntimeException {
+    public UnsupportedExtensionException(String message) {
+        super(message);
+    }
+}
--- src/main/java/pl/epam/challenge/util/FileSignatureChecker.java
+++ src/main/java/pl/epam/challenge/util/FileSignatureChecker.java
@@ -1,8 +1,37 @@
 package pl.epam.challenge.util;
 
+import pl.epam.challenge.exceptions.UnsupportedExtensionException;
+
+import java.util.Map;
+
 public class FileSignatureChecker {
 
+    // Map.of() used for readability, if number of keys grows beyond 10
+    // can replace with Arrays.stream().collect(Collectors.toMap())
+    public static final Map<String, String> KNOWN_SIGNATURES = Map.of(
+            "jpg", "ff d8 ff",
+            "gif", "47 49 46 38",
+            "png", "89 50 4e 47 0d 0a 1a 0a",
+            "exe", "4d 5a",
+            "ogg", "4f 67 67 53",
+            "mp3", "ff fb",
+            "pdf", "25 50 44 46 2d"
+    );
+
     public String fileChecker(String filename) {
-        return filename;
+
+        String currentFileExtension = FileUtils
+                .getFileExtension(filename)
+                .orElse("File extension not recognized.");
+
+        switch (currentFileExtension) {
+            case "jpg":
+            case "gif":
+                return "ok";
+            case "txt":
+                return "ok txt";
+            default:
+                throw new UnsupportedExtensionException(currentFileExtension);
+        }
     }
 }
